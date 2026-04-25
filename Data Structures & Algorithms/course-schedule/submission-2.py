6e7902c068fadb11eb90cc2bd4660d64f4e761cf
Akshay Viswanath <akshayviswanathgrad@gmail.com>
2026-04-25 13:44:11 -0400

    Add: course-schedule - submission-2

---
+++ Data Structures & Algorithms/course-schedule/submission-2.py
@@ -0,0 +1,29 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        hash = {i:[] for i in range(numCourses)}
+        visit = set()
+        for i,j in (prerequisites):
+            hash[i].append(j)
+            
+
+        def dfs(i):
+            if hash[i] == []:
+                return True
+            if i in visit:
+                return False
+            visit.add(i)
+            for j in hash[i]:
+                if not dfs(j):
+                    return False
+            
+            return True
+        
+        for i in range(numCourses):
+            if not dfs(i):
+                return False
+        
+        return True
+
+            
+             
+        
