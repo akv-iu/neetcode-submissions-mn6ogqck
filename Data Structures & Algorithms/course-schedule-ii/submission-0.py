09bc4ceb79c34807b050212cb4d0abdff70c7bad
Akshay Viswanath <akshayviswanathgrad@gmail.com>
2026-04-25 14:01:37 -0400

    Add: course-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-0.py
@@ -0,0 +1,29 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        hash = {i:[] for i in range(numCourses)}
+        for i,j in prerequisites:
+            hash[i].append(j)
+        
+        visit = set()
+        res = []
+        def dfs(curr):
+            if hash[curr] == []:
+                print(curr)
+                res.append(curr)
+                print(res)
+                return res
+            if curr in visit:
+                return 
+            visit.add(curr)
+            for i in hash[curr]:
+                if not dfs(i): return 
+            visit.remove(curr)  
+            res.append(curr)  
+            return res
+        
+        for i in range(numCourses):
+            if not dfs(i):return []
+        
+        return res
+
+        
