a5b2892fdc584a4d38b419493664225b89d52476
Akshay Viswanath <akshayviswanathgrad@gmail.com>
2026-04-29 09:28:33 -0400

    Add: reconstruct-flight-path - submission-2

---
+++ Data Structures & Algorithms/reconstruct-flight-path/submission-2.py
@@ -0,0 +1,29 @@
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        dict = collections.defaultdict(list)
+        for src,dest in tickets:
+            dict[src].append(dest)
+        
+        n = len(tickets)
+
+        for src in dict:
+            dict[src].sort()
+        ans = ["JFK"]
+        
+        def dfs(curr):
+            if len(ans) == n+1:
+                return True
+            for i, dest in enumerate(dict[curr]):
+                temp = dict[curr].pop()
+                ans.append(temp)
+                if dfs(dest):
+                    return True
+                ans.pop()
+                dict[curr].insert(i,temp)
+            
+            return False
+        
+        if dfs("JFK"):
+            return ans
+        return -1
+                       
