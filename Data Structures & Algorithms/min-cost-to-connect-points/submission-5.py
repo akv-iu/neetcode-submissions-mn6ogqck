77a70b872f1228e4fc18fb2fa04b6c8959181367
Akshay Viswanath <akshayviswanathgrad@gmail.com>
2026-04-30 10:08:41 -0400

    Add: min-cost-to-connect-points - submission-5

---
+++ Data Structures & Algorithms/min-cost-to-connect-points/submission-5.py
@@ -0,0 +1,30 @@
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        n = len(points) - 1
+        visit = set()
+        
+        ans = 0
+
+        def bfs(src,ans,edg,local_visit):
+            if edg == n:
+                
+                return ans
+            visit.add((src[0],src[1]))
+            local_visit.append([src[0],src[1]])
+            arr = []
+            for j in range(len(local_visit)):
+                for i in range(len(points)):
+                    if tuple(points[i]) not in visit:
+                        dist = abs(local_visit[j][0] - points[i][0]) + abs(local_visit[j][1] - points[i][1])
+                        arr.append([dist,i])
+            dist, index = heapq.heappop(arr)
+            ans += dist
+            edg+= 1
+            
+            return bfs(points[index],ans,edg,local_visit)
+        src = points.pop(0)
+        
+        return bfs(src,0,0,[])
+
+
+        
