e20abf1e57ccc418bd4872f215a0c955f0930f91
Akshay Viswanath <akshayviswanathgrad@gmail.com>
2026-05-25 11:23:48 -0400

    Add: two-integer-sum - submission-8

---
+++ Data Structures & Algorithms/two-integer-sum/submission-8.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hash = {}
+        for i in range(len(nums)):
+            
+            diff = target - nums[i]
+            
+            if diff in hash:
+                return([hash[diff],i])
+            hash[nums[i]] = i
+        
+        return []
+        
