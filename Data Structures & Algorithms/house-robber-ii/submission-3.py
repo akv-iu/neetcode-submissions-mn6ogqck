5dfcd210fb47849e9130f6839b702d94adb3565e
Akshay Viswanath <akshayviswanathgrad@gmail.com>
2026-09-16 19:23:40 -0600

    Add: house-robber-ii - submission-3

---
+++ Data Structures & Algorithms/house-robber-ii/submission-3.py
@@ -0,0 +1,24 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        
+
+        def rob2(nums):
+            n = len(nums)
+            print(nums)
+            curr = 0
+            nums[1] = max(nums[0],nums[1])
+            for i in range(2,n):
+                nums[i] = max(curr,nums[i] + nums[i-2])
+                curr = max(curr,nums[i])
+            
+            return nums[n-1]
+        n = len(nums)
+        first = rob2(nums[1:])
+        second = rob2(nums[:n-1])
+
+        return (max(first,second))
+                
+
+
+
+        
